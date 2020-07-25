a58b8778474b1361be837b277c03ca84974aed75
Liang, Kui <liang.kui@hotmail.com>
2020-07-25 11:19:19 +0800

    feat(client): modify userpool id

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,7 +1,7 @@
 export const CONFIG = {
   cognito: {
     REGION: 'ap-northeast-1',
-    USER_POOL_ID: 'ap-northeast-1_VjzewlkNy',
-    APP_CLIENT_ID: '3mepta92g3a4au4bkvk9fptn64'
+    USER_POOL_ID: 'ap-northeast-1_u0p9DVjHw',
+    APP_CLIENT_ID: '3movnodvgs6btgejehb2lrgccs'
   }
 }

4a5e141dab4b829a1143e86a7789ba20eb7d83f0
Liang, Kui <liang.kui@hotmail.com>
2020-07-28 16:57:13 +0800

    deploy v1.0.0

---
+++ build/precache-manifest.6e56111ff84aa274506846de5ae185af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53b129a9bcc45e7a1e296294143120e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a6b15e446f149e4d85a3",
+    "url": "/static/css/main.f052c464.chunk.css"
+  },
+  {
+    "revision": "245fcd9d244d96dd874b",
+    "url": "/static/js/2.82fbf164.chunk.js"
+  },
+  {
+    "revision": "31822c2e19ed7b4796192330118d6914",
+    "url": "/static/js/2.82fbf164.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6b15e446f149e4d85a3",
+    "url": "/static/js/main.4529398b.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  }
+]);

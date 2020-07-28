5b9592b868113cd45bb160c424862e8c1df5e598
Liang, Kui <liang.kui@hotmail.com>
2020-07-28 17:12:09 +0800

    chore: add deploy script

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+git checkout deploy
+
+rm -rf ./build
+
+cd client && yarn build
+
+mv build ..
+
+git add ../*
+
+version="$(date)"
+
+git cm "$version"
+
+git push origin deploy
+
+git checkout master
